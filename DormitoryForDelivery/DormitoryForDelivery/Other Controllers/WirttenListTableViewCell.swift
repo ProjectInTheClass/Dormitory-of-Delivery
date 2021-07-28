d7b67fda9874d7ff0cf2965cfc1171da010f520b
samdong3298 <86140341+samdong3298@users.noreply.github.com>
2021-07-28 22:41:37 +0900

    7/28

---
+++ DormitoryForDelivery/DormitoryForDelivery/Other Controllers/WirttenListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WirttenListTableViewCell.swift
+//  DormitoryForDelivery
+//
+//  Created by 김동현 on 2021/07/27.
+//
+
+import UIKit
+
+class WirttenListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
