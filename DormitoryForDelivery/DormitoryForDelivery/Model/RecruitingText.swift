03feac27f2298da41707504579542f5af77acf5b
lxodud <xodud1998223@naver.com>
2021-08-02 23:51:10 +0900

    메인화면 UI작업

---
+++ DormitoryForDelivery/DormitoryForDelivery/Model/RecruitingText.swift
@@ -0,0 +1,15 @@
+//
+//  RecruitingText.swift
+//  DormitoryForDelivery
+//
+//  Created by 이태영 on 2021/08/02.
+//
+
+import Foundation
+
+struct RecruitingText {
+    let symbol: String
+    let postTitle: String
+    let categories: String
+    let totalNumber: Int
+}
---
+++ DormitoryForDelivery/DormitoryForDelivery/View Controllers/MainTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  MainTableViewController.swift
+//  DormitoryForDelivery
+//
+//  Created by 이태영 on 2021/08/02.
+//
+
+import UIKit
+
+
+class MainTableViewController: UITableViewController {
+
+    var mainPosts: [RecruitingText] = [
+        RecruitingText(symbol: "🍔", postTitle: "7시 30분에 햄버거 먹을 사람 5동만", categories: "#햄버거, #맥날", totalNumber: 2),
+        RecruitingText(symbol: "🍕", postTitle: "피자 시켜먹을 분", categories: "#피자", totalNumber: 2),
+        RecruitingText(symbol: "🧇", postTitle: "와플 같이먹을 분", categories: "#디저트", totalNumber: 3),
+        RecruitingText(symbol: "🍣", postTitle: "7시에 초밥 같이먹을 분", categories: "#일식", totalNumber: 2)]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return mainPosts.count
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+
+        let mainPost = mainPosts[indexPath.row]
+        
+        cell.update(with: mainPost)
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
