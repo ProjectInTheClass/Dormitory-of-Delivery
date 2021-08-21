1d61e7387029d2637b578cbe046306de37b1591c
lxodud <xodud1998223@naver.com>
2021-08-22 00:57:15 +0900

    포스트뷰 참여버튼 alert컨트롤러 구현

--- DormitoryForDelivery/DormitoryForDelivery/View Controllers/PostViewController.swift
+++ DormitoryForDelivery/DormitoryForDelivery/View Controllers/PostViewController.swift
@@ -9,14 +9,15 @@ import UIKit
 
 class PostViewController: UIViewController {
     
-    @IBOutlet weak var participateButtonView: UIView!
+    @IBOutlet weak var includeparticipateButtonView: UIView!
+    
     
     var mainPostInformation: RecruitingText?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        participateButtonView.layer.addBorder([.top], color: UIColor.gray, width: 1.0)
+        includeparticipateButtonView.layer.addBorder([.top], color: UIColor.gray, width: 1.0)
         
         // 네비게이션바 디자인 설정
         navigationController?.navigationBar.shadowImage = UIImage()
@@ -25,6 +26,16 @@ class PostViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func participateButtonTapped(_ sender: Any) {
+        let participateAlertController = UIAlertController(title: "주문에 참여하시겠습니까?", message: "주문 참여 시 채팅방에 초대됩니다.", preferredStyle: .alert)
+        let alertCancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        let alertOkayAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+        participateAlertController.addAction(alertCancelAction)
+        participateAlertController.addAction(alertOkayAction)
+        present(participateAlertController, animated: true, completion: nil)
+    }
+    
     
     // MARK: - Navigation
 
