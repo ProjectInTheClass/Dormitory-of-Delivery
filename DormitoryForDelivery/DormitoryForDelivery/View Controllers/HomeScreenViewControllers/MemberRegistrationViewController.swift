0ac0311b066a678e1a4325d77ddfe0d96802fe32
samdong3298 <86140341+samdong3298@users.noreply.github.com>
2022-02-27 01:49:18 +0900

    회원가입UI 키보드 notification 설정(미완성)

--- DormitoryForDelivery/DormitoryForDelivery/View Controllers/HomeScreenViewControllers/MemberRegistrationViewController.swift
+++ DormitoryForDelivery/DormitoryForDelivery/View Controllers/HomeScreenViewControllers/MemberRegistrationViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import FirebaseAuth
 import FirebaseFirestore
 
-class MemberRegistrationViewController: UIViewController, UITextFieldDelegate {
+class MemberRegistrationViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
     
     @IBOutlet weak var emailTextField: UITextField!{ didSet {
         emailTextField.delegate = self
@@ -31,21 +31,40 @@ class MemberRegistrationViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var memberRegistrationButton: UIButton!
     
-   
+    @IBOutlet weak var scrollView: UIScrollView!
     
+    @IBAction func contentViewTapped(_ sender: Any) {
+        emailTextField.resignFirstResponder()
+        passwordTextField.resignFirstResponder()
+        studentNumberTextField.resignFirstResponder()
+        userNameTextField.resignFirstResponder()
+    }
     
     let db:Firestore = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationUI()
-        
-        }
+        scrollView.delegate = self
+        try! Auth.auth().signOut()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
     
     @IBAction func memberRegistrationButtonTapped(_ sender: Any) {
         let password = passwordTextField.text!
-        let email = emailTextField.text!
-        let newUser = ["email":email]
+        let email = emailTextField.text! + "@gs.cwnu.ac.kr"
+        let studentNumber = studentNumberTextField.text!
+        let userName = userNameTextField.text!
+        let newUser = ["email":email, "studentNumber":studentNumber, "userName":userName]
         
         guard emailTextField.text?.isEmpty == false, passwordTextField.text?.isEmpty == false, checkPasswordTextField.text?.isEmpty == false else {
             let alertController = UIAlertController(title: "이메일과 비밀번호를 작성해주세요.", message: nil, preferredStyle: .alert)
@@ -63,26 +82,23 @@ class MemberRegistrationViewController: UIViewController, UITextFieldDelegate {
             return
         }
         
-//        guard Auth.auth().currentUser != nil else {
-//            let alertController = UIAlertController(title: "이메일인증을 해주세요.", message: nil, preferredStyle: .alert)
-//            let checkAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-//            alertController.addAction(checkAction)
-//            self.present(alertController, animated: true, completion: nil)
-//            return
-//        }
+        guard userNameTextField.text?.isEmpty == false, studentNumberTextField.text?.isEmpty == false else {
+            let alertController = UIAlertController(title: "학번과 이름을 작성해주세요", message: nil, preferredStyle: .alert)
+            let checkAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+            alertController.addAction(checkAction)
+            present(alertController, animated: true, completion: nil)
+            return
+        }
         
-        
-        db.collection("users").whereField("email", isEqualTo: emailTextField.text!).getDocuments { (snapshot: QuerySnapshot?, error) in
+        db.collection("users").whereField("email", isEqualTo: email).getDocuments { (snapshot: QuerySnapshot?, error) in
+            // 중복된 이메일이 없을경우
             if snapshot!.documents.count == 0 {
                 Auth.auth().createUser(withEmail: email, password: password, completion: { (snapshot,error) in
+                    // 이메일과 비밀번호로 계정을 생성한후 오류가 없을시 데이터베이스에 정보도 입력
                     if error == nil {
                         self.db.collection("users").document(Auth.auth().currentUser!.uid).setData(newUser)
-                        
-                        if let uid = FirebaseDataService.instance.currentUserUid {
-                            FirebaseDataService.instance.userRef.child(uid).child("email").setValue(email)
-                        }
                         self.performSegue(withIdentifier: "emailCertification", sender: nil)
-                        
+                    // 계정생성시 오류발생
                     } else {
                         let alertController = UIAlertController(title: "이메일 형식이 올바르지 않습니다.", message: nil, preferredStyle: .alert)
                         let checkAction = UIAlertAction(title: "확인", style: .default, handler: nil)
@@ -90,30 +106,69 @@ class MemberRegistrationViewController: UIViewController, UITextFieldDelegate {
                         self.present(alertController, animated: true, completion: nil)
                     }
                 })
+            // 중복된 이메일이 존재할 경우  OR  이메일인증화면에서 뒤로왔을 경우
             } else {
-                let alertController = UIAlertController(title: "이미 존재하는 이메일입니다.", message: nil, preferredStyle: .alert)
-                let checkAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-                alertController.addAction(checkAction)
-                self.present(alertController, animated: true, completion: nil)
+                guard let uid = Auth.auth().currentUser?.uid else {
+                    //중복된 이메일이 존재할경우
+                    let alertController = UIAlertController(title: "이미 존재하는 이메일입니다.", message: nil, preferredStyle: .alert)
+                    let checkAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+                    alertController.addAction(checkAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    return
+                }
+                // 이메일인증화면에서 뒤로왔을 경우
+                let ref = self.db.collection("users").document(uid)
+                ref.getDocument { (document, error) in
+                    guard let document = document else { return }
+                    let value = document.data()!["email"] as! String
+                    if value == email {
+                        self.performSegue(withIdentifier: "emailCertification", sender: nil)
+                    }
+                }
             }
         }
     }
     
     
     @IBAction func dismissButtonTapped(_ sender: Any) {
-        //Auth.auth().currentUser?.delete()
         dismiss(animated: true, completion: nil)
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        view.endEditing(true)
-    }
-    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         emailTextField.resignFirstResponder()
         passwordTextField.resignFirstResponder()
-        checkPasswordTextField.resignFirstResponder()
+        studentNumberTextField.resignFirstResponder()
+        userNameTextField.resignFirstResponder()
         return true
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        let height = getKeyboardHeight(notification)
+        view.frame.origin.y = -height
+        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height)
+        scrollView.contentInset.top = height
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+        scrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        scrollView.contentInset.top = 0
+    }
+    
+    func getKeyboardHeight(_ notification: Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func navigationUI() {
@@ -127,7 +182,7 @@ class MemberRegistrationViewController: UIViewController, UITextFieldDelegate {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "emailCertification" {
             let destinationViewController = segue.destination as? EmailCertificationViewController
-            destinationViewController?.emailText = emailTextField.text
+            destinationViewController?.emailText = emailTextField.text! + "@gs.cwnu.ac.kr"
         }
     }
     
